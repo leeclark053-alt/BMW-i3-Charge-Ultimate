a91505f31d0fc39fa3e2c798767a1797e00ce34b
leeclark053-alt <leeclark053@googlemail.com>
2026-04-29 13:26:26 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+self.addEventListener("install", e => {
+
+  e.waitUntil(
+
+    caches.open("ev-cache").then(cache => {
+
+      return cache.addAll(["./"]);
+
+    })
+
+  );
+
+});
+
+self.addEventListener("fetch", e => {
+
+  e.respondWith(
+
+    caches.match(e.request).then(res => res || fetch(e.request))
+
+  );
+
+});
